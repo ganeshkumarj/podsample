96f25f489d7b6dc4feb1bc183636e525727739b9
ganesh <ganeshkumarj@live.com>
2020-08-09 17:49:35 +0530

    pod example2

--- RESTfulExample/src/main/java/com/mkyong/rest/HelloWorldService.java
+++ RESTfulExample/src/main/java/com/mkyong/rest/HelloWorldService.java
@@ -3,9 +3,12 @@ package com.mkyong.rest;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
  
 @Path("/pod")
+@Produces(MediaType.APPLICATION_JSON)
 public class HelloWorldService {
  
 	@GET
